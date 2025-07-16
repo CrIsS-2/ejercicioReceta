339d9fec5f60a6349fa525bac76f027db7120d80
CrIsS-2 <gomezcristopher471@gmail.com>
2025-07-16 10:06:40 -0500

    Update recetaControlador.js

--- controller/recetaControlador.js
+++ controller/recetaControlador.js
@@ -1,17 +1,22 @@
+// CONTROLADOR - puente entre modelo y vista
 function crearControlador(receta, vista) {
   return {
+    // Cambia el nombre de la receta en el modelo
     actualizarNombre(nombre) {
       receta.setNombre(nombre);
     },
 
+    // Cambia la descripción de la receta en el modelo
     actualizarDescripcion(desc) {
       receta.setDescripcion(desc);
     },
 
+    // Actualiza la vista mostrando los datos del modelo
     mostrar() {
       vista.mostrarReceta(receta.getNombre(), receta.getDescripcion());
     }
   };
 }
 
+// Exportamos el controlador para usarlo en el programa principal
 module.exports = { crearControlador };
