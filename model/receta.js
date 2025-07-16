7e6f50ec17a69324cf41953d32170aaaca2d7dc4
CrIsS-2 <gomezcristopher471@gmail.com>
2025-07-16 10:07:27 -0500

    Update receta.js

--- model/receta.js
+++ model/receta.js
@@ -1,24 +1,30 @@
+// MODELO - clase Receta: representa los datos de una receta
 class Receta {
   constructor(nombre, descripcion) {
-    this.nombre = nombre;
-    this.descripcion = descripcion;
+    this.nombre = nombre;           // Nombre de la receta
+    this.descripcion = descripcion; // Descripción o ingredientes
   }
 
+  // Setter: actualiza el nombre
   setNombre(nombre) {
     this.nombre = nombre;
   }
 
+  // Setter: actualiza la descripción
   setDescripcion(descripcion) {
     this.descripcion = descripcion;
   }
 
+  // Getter: obtiene el nombre
   getNombre() {
     return this.nombre;
   }
 
+  // Getter: obtiene la descripción
   getDescripcion() {
     return this.descripcion;
   }
 }
 
+// Exportamos la clase para usarla en otros archivos
 module.exports = Receta;
